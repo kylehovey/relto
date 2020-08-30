c6e8edda088ba382552e7cee4e141629e7d74037
khoveyc@gmail.com <khoveyc@gmail.com>
2020-08-30 14:43:28 -0600

    Created initial site design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+
+import './App.scss';
+
+import projects from './data/projects';
 
 function App() {
+  const methods = {
+    get projects() {
+      return projects.map(({ title, brief, link }, i) => (
+        <li key={`project-${i}`}>
+          <a href={link} target="_blank">
+            <span className="title">{title}</span>
+            <span className="brief">{brief}</span>
+          </a>
+        </li>
+      ));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <div className="main">
+          <div className="description">
+            <a class="logo" href="#"></a>
+            <h2>Welcome to Relto</h2>
+            <p>This is Relto, Kyle Hovey's project portfolio. Here you will find links to projects and diatribes on math and science. Check out my blog if you would like to see more long-form stuff. This is Relto, Kyle Hovey's project portfolio. Here you will find links to projects and diatribes on math and science. Check out my blog if you would like to see more long-form stuff.</p>
+          </div>
+          <ul className="projects-list">
+            {methods.projects}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
