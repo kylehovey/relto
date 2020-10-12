776786ba7c1f286685110abd68ce3c4cd4f41b67
Kyle Hovey <sudokyle@gmail.com>
2020-10-11 20:24:07 -0600

    Changed copy on front page

--- src/App.js
+++ src/App.js
@@ -107,8 +107,8 @@ function App() {
         <div className="main">
           <div className="description">
             <a class="logo" href="#"></a>
-            <h2>Welcome to Relto</h2>
-            <p>This is Relto, Kyle Hovey's project portfolio. Here you will find links to projects and diatribes on math and science. Check out my blog if you would like to see more long-form content on my interests.</p>
+            <h2>Project Portfolio</h2>
+            <p>Hi! My name is Kyle. I grew up in a small community in the Sierra Nevada mountains. In 2019, I graduated Utah State University with a BS in Computational Mathematics. My passions include mathematics, electrical engineering, programming, music, hiking, climbing, and philosophy. Here you can see some highlights from my past/current projects. Check out my blog for diatribes on math and programming.</p>
           </div>
           {methods.projects}
           {methods.description}
